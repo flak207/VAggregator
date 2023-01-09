0f541d9164b94aeb6c7114de24cedff581bae089
Komissarov Eugene <komissarovea@hotmail.com>
2023-01-09 23:48:11 +0300

    wrap panel sample

---
+++ KEA.VAggregator.Mobile/KEA.VAggregator.Mobile/ImageList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KEA.VAggregator.Mobile
+{
+    class ImageList
+    {
+        public List<string> Photos = null;
+    }
+}
--- KEA.VAggregator.Mobile/KEA.VAggregator.Mobile/MainPage.xaml.cs
+++ KEA.VAggregator.Mobile/KEA.VAggregator.Mobile/MainPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.CommunityToolkit.UI.Views;
@@ -11,18 +14,46 @@ namespace KEA.VAggregator.Mobile
 {
     public partial class MainPage : ContentPage
     {
+        HttpClient _client = new HttpClient();
+
         public MainPage()
         {
             InitializeComponent();
         }
 
-        void OnMediaOpened(object sender, EventArgs e) => Console.WriteLine("Media opened.");
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
 
-        void OnMediaFailed(object sender, EventArgs e) => Console.WriteLine("Media failed.");
+            var images = await GetImageListAsync();
+            if (images != null)
+            {
+                foreach (var photo in images.Photos)
+                {
+                    var image = new Image
+                    {
+                        Source = ImageSource.FromUri(new Uri(photo))
+                    };
+                    wrapLayout.Children.Add(image);
+                }
+            }
+        }
 
-        void OnMediaEnded(object sender, EventArgs e) => Console.WriteLine("Media ended.");
+        async Task<ImageList> GetImageListAsync()
+        {
+            try
+            {
+                string requestUri = "https://raw.githubusercontent.com/xamarin/docs-archive/master/Images/stock/small/stock.json";
+                string result = await _client.GetStringAsync(requestUri);
+                return JsonConvert.DeserializeObject<ImageList>(result);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"\tERROR: {ex.Message}");
+            }
 
-        void OnSeekCompleted(object sender, EventArgs e) => Console.WriteLine("Seek completed.");
+            return null;
+        }
 
     }
 }
